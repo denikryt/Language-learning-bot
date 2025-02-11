6743497be4442c1f42a25238469de850949b4519
denikryt <danil.mirzoev@gmail.com>
2025-02-12 00:04:17 +0200

    improvements for guess word game

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -27,15 +27,16 @@ class Game():
     test_window = 0
     result_window = 0
     translation_mark = 5
+    shuffle_mark = 10
 
     guessing = False
     test = False
-    added_translation = False
+    help_add_translation = False
+    help_shuffle_letters = False
 
     definition_instructions = "Provide an explanation and definition of the word only.\
         Give an aswer in a simple format, without any additional information or formatting. \
         Do not write the word itself in the answer."
-    
     
 
     def inline_buttons(self, message=None, call=None):
@@ -80,10 +81,17 @@ class Game():
         if call.data == 'finish':
             self.test = False
             self.testing = False
+
             self.send_finish_message(message, call)
 
+        if call.data == 'shuffle':
+            self.help_shuffle_letters = True
+            self.shuffle_mark = 0
+            bot.edit_message_reply_markup(chat_id=user_data['user_id'], message_id=user_data['message_id'], reply_markup=self.inline_game_buttons())
+            self.send_letters_message(message, call)
+
         if call.data == 'add_translation':
-            self.added_translation = True
+            self.help_add_translation = True
             self.translation_mark = 0
             self.send_correct_message(message, call)
             
@@ -140,14 +148,18 @@ class Game():
 
     def start(self, message=None, call=None):
         self.loose = False
-        self.added_translation = False
+        self.help_add_translation = False
+        self.help_shuffle_letters = False
         self.translation_mark = 5
+        self.shuffle_mark = 10
         self.attempts = 3
         self.help = 3
         self.char = 0
         self.spelling = ''
         self.chars = []
         self.chars = [char.lower() for char in self.random_word]
+        self.keyboard_chars = self.chars.copy()
+        self.keyboard_chars = [char for char in self.keyboard_chars if char != ' ']
         self.stars = []
         self.stars = ['*' if not x == ' ' else ' ' for x in self.random_word]
 
@@ -155,18 +167,18 @@ class Game():
         self.send_start_message(message, call)
 
     def game_text(self):
-        if self.added_translation:
+        if self.help_add_translation:
             text = f'Guess the word\n<b>{self.definition}</b>\nTranslation: <b>{self.translate}</b>\n{self.spelling}\nAttempts: {str(self.attempts)}\nHints: {str(self.help)}'
         else:
             text = f'Guess the word\n<b>{self.definition}</b>\n{self.spelling}\nAttempts: {str(self.attempts)}\nHints: {str(self.help)}'
         return text
     
     def loose_text(self):
-        text = f'You loose!\n<b>{self.definition}</b>\ndescribes the word\n<b>{self.random_word}</b>\nTranslation: <b>{self.translate}</b>'
+        text = f'You loose!\n<b>{self.definition}</b>\ndescribes the word\n<b>{self.random_word}</b>\nTranslation: <b>{self.translate}</b>\nExample:\n<b>{self.random_word_example}</b>'
         return text
     
     def win_text(self):
-        text = f'Correct!\n<b>{self.definition}</b>\ndescribes the word\n<b>{self.random_word}</b>\nTranslation: <b>{self.translate}</b>'
+        text = f'Correct!\n<b>{self.definition}</b>\ndescribes the word\n<b>{self.random_word}</b>\nTranslation: <b>{self.translate}</b>\nExample:\n<b>{self.random_word_example}</b>'
         return text
     
     def finish_text(self):
@@ -181,7 +193,13 @@ class Game():
         item2 = types.InlineKeyboardButton('give up', callback_data='give_up')
         item3 = types.InlineKeyboardButton('translation', callback_data='add_translation')
         item4 = types.InlineKeyboardButton('save as text', callback_data='save_text')
-        inline_markup.add(item1, item2, item3, item4)
+        item5 = types.InlineKeyboardButton('shuffle letters', callback_data='shuffle')
+        
+        if self.help_shuffle_letters:
+            inline_markup.add(item1, item2, item3, item4)
+        else:
+            inline_markup.add(item1, item2, item3, item4, item5)
+
         return inline_markup
     
     def inline_end_buttons(self):
@@ -191,33 +209,41 @@ class Game():
         inline_markup.add(item1, item2)
         return inline_markup
 
+    def send_letters_message(self, message=None, call=None):
+        user_data = self.get_user_data(message, call)
+
+        keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        items = [types.KeyboardButton(item) for item in self.keyboard_chars]
+        random.shuffle(items)
+        keyboard_markup.add(*items)
+
+        msg = bot.send_message(user_data['user_id'], 'Choose the letter', reply_markup=keyboard_markup, parse_mode='html')
+        self.letters_message = msg.message_id
+        self.last_message_id = msg.message_id
+
+    def send_start_message(self, message=None, call=None):
+        user_data = self.get_user_data(message, call)
+
+        inline_markup = self.inline_game_buttons()
+                
+        text = self.game_text()
+        msg = bot.send_message(user_data['user_id'], text, reply_markup=inline_markup, parse_mode='html')
+        self.game_window = msg.message_id
+
+        self.last_message_id = msg.message_id
+
+        bot.edit_message_reply_markup
+
     def send_win_message(self, message=None, call=None):
         user_data = self.get_user_data(message, call)
 
         inline_markup = self.inline_end_buttons()
         text = self.win_text()
 
-        bot.delete_message(user_data['user_id'],message_id=self.keyboard_message)
+        if self.help_shuffle_letters:
+            bot.delete_message(user_data['user_id'],message_id=self.letters_message)
+        
         bot.edit_message_text(chat_id=user_data['user_id'], message_id=self.game_window, text=text, reply_markup=inline_markup, parse_mode='html')
-
-    def send_start_message(self, message=None, call=None):
-        user_data = self.get_user_data(message, call)
-
-        inline_markup = self.inline_game_buttons()
-
-        keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        items = [types.KeyboardButton(item) for item in self.random_word]
-        random.shuffle(items)
-        keyboard_markup.add(*items)
-                
-        text = self.game_text()
-        msg = bot.send_message(user_data['user_id'], text, reply_markup=inline_markup, parse_mode='html')
-        self.game_window = msg.message_id
-
-        msg = bot.send_message(user_data['user_id'], 'Choose the letter', reply_markup=keyboard_markup, parse_mode='html')
-        self.keyboard_message = msg.message_id
-
-        self.last_message_id = msg.message_id
 
     def send_correct_message(self, message=None, call=None):
         user_data = self.get_user_data(message, call)
@@ -225,13 +251,16 @@ class Game():
         inline_markup = self.inline_game_buttons()
         text = self.game_text()
 
+        if self.help_shuffle_letters:
+            bot.delete_message(chat_id=user_data['user_id'], message_id=self.letters_message)
+            self.send_letters_message(message, call)
+
         bot.edit_message_text(chat_id=user_data['user_id'], message_id=self.game_window, text=text, reply_markup=inline_markup, parse_mode='html')
-    
+
     def send_incorrect_message(self, message=None, call=None):
         user_data = self.get_user_data(message, call)
         
         inline_markup = self.inline_game_buttons()
-
         text = self.game_text()
 
         bot.edit_message_text(chat_id=user_data['user_id'], message_id=self.game_window, text=text, reply_markup=inline_markup, parse_mode='html')
@@ -242,7 +271,9 @@ class Game():
         inline_markup = self.inline_end_buttons()
         text = self.loose_text()
 
-        bot.delete_message(user_data['user_id'],message_id=self.keyboard_message)
+        if self.help_shuffle_letters:
+            bot.delete_message(user_data['user_id'],message_id=self.letters_message)
+        
         bot.edit_message_text(chat_id=user_data['user_id'], message_id=self.game_window, text=text, reply_markup=inline_markup, parse_mode='html')
 
     def send_finish_message(self, message=None, call=None):
@@ -277,7 +308,7 @@ class Game():
             self.testing = False
             self.spelling = word
 
-            new_mark = self.attempts + self.help + self.translation_mark
+            new_mark = self.attempts + self.help + self.translation_mark + self.shuffle_mark
             db.update_word_level(word=self.random_word, collection_name=str(user_data['user_id']), level=new_mark + self.mark)
 
             self.guessed.append(f"<b>{self.random_word}</b> : {str(self.mark)} +{str(new_mark)}")
@@ -288,11 +319,12 @@ class Game():
             self.stars[self.char] = char
             self.spelling = ''.join(self.stars)
             self.char += 1
+            self.keyboard_chars.remove(char)
 
             if self.spelling == word:
                 self.testing = False
 
-                new_mark = self.attempts + self.help + self.translation_mark
+                new_mark = self.attempts + self.help + self.translation_mark + self.shuffle_mark
                 db.update_word_level(word=self.random_word, collection_name=str(user_data['user_id']), level=new_mark + self.mark)
 
                 self.guessed.append(f"<b>{self.random_word}</b> : {str(self.mark)} +{str(new_mark)}")
